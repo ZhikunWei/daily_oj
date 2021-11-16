ecb91a6dceb7e734ff46da166e33c0024f576a42
Zhikun Wei <1045525044@qq.com>
2021-11-16 16:21:18 +0800

    done. 数重合顶点个数，判断总面积，确定最外侧顶点只出现一次

--- 20211116-391.py
+++ 20211116-391.py
@@ -11,9 +11,12 @@ class Solution:
             area_sum += (a-x)*(b-y)
             max_point = [min(max_point[0], x), min(max_point[1], y), max(max_point[2], a), max(max_point[3], b)]
         cnt = 0
+        print(point_dict)
         for p in point_dict:
             if point_dict[p] == 1:
                 cnt += 1
+            elif point_dict[p] != 2 and point_dict[p] != 4:
+                return False
         real_area = (max_point[2] - max_point[0]) * (max_point[3] - max_point[1])
         for p in [(max_point[0], max_point[1]), (max_point[0], max_point[3]), (max_point[2], max_point[3]), (max_point[2], max_point[1])]:
             if p not in point_dict or point_dict[p] != 1:
@@ -21,5 +24,5 @@ class Solution:
         return cnt == 4 and real_area == area_sum
 
 
-t = Solution().isRectangleCover([[0,0,1,1],[0,1,1,2],[0,2,1,3],[1,0,2,1],[1,0,2,1],[1,2,2,3],[2,0,3,1],[2,1,3,2],[2,2,3,3]])
+t = Solution().isRectangleCover([[1,1,3,3],[3,1,4,2],[3,2,4,4],[1,3,2,4],[2,3,3,4]])
 print(t)
